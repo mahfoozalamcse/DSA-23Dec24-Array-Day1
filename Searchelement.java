a3a638de18a895872ba812cd57f9745397ec1ff8
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-12-25 04:56:04 +0000

    practice

---
+++ Searchelement.java
@@ -0,0 +1,18 @@
+public class Searchelement {
+
+    public static void search(int arr[], int key){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == key){
+                System.out.println("Element found at index " + i + " " + arr[i]);
+                return;
+            }
+        }
+        System.out.println("Element not found");
+    }
+    public static void main(String[] args) {
+        int arr[] = {12, 78, 90, 96};
+        int key = 78;
+        search(arr, key);
+        
+    }
+}
